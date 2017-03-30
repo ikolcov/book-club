1ab7364e8553861e1bc5af61e13864747977f048
ikolcov <ikolcov@yandex.ru>
2017-03-30 11:13:33 +0300

    try

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-              localIdentName: '[name]__[local]__[hash:base64:5]',
+              localIdentName: '[local]__[hash:base64:5]',
             },
           },
           'sass-loader',
